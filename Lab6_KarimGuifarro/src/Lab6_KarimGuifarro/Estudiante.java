ccd4de78a783f95188ed299715ac947d60aee3d4
KaOz25 <karimozael@gmail.com>
2020-11-20 14:32:53 -0600

    primer commit

---
+++ Lab6_KarimGuifarro/src/Lab6_KarimGuifarro/Estudiante.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab6_KarimGuifarro;
+
+/**
+ *
+ * @author Karim Ozael
+ */
+public class Estudiante {
+    private String nombre;
+    private String apellido;
+    private String num_cuenta;
+    private String edad;
+    private String carrera;
+    
+}
